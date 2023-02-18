98031dcdaa1d2498767d082c709f5ac769e607df
ge.tang <tangge@vip.qq.com>
2023-02-18 18:04:15 +0800

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [ //那些属于本 NgModule 的组件、指令、管道。
+    AppComponent
+  ],
+  imports: [      //依赖其他模块
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],  //服务
+  bootstrap: [AppComponent], //应用的主视图，称为根组件。它是应用中所有其它视图的宿主。只有根模块才应该设置这个 bootstrap 属性。
+})
+export class AppModule { }

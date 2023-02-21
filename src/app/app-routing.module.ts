13f9ae4348bf79641a6f25e8434a955f59fcb286
ge.tang <tangge@vip.qq.com>
2023-02-21 23:11:57 +0800

    2-4 【接口Interface Flex布局】让顶部导航滚动

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { ContentChild, NgModule } from '@angular/core';
 import { RouterModule, Routes, ActivatedRoute } from '@angular/router';
-import { ContentComponent } from './share/content/content.component';
+
 
 
 const rootRouterConfig: Routes = [
-  { path:'**', component: ContentComponent },
-  // { path:'list/:id', component: ListComponent },
-  { path:'list', loadChildren: ()=> import('./share/children-module/children-module.module').then(m=>m.ChildrenModuleModule) },
-  { path:'content', component: ContentComponent },
-  { path: 'children-module', loadChildren: () => import('./share/children-module/children-module.module').then(m => m.ChildrenModuleModule) },
+
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface TopMemu {
+  title: string,
+  link?: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pinduoduo';
+  tabs: TopMemu[] = [{
+    title: "热门"
+  }, {
+    title: "男装"
+  }, {
+    title: "手机"
+  }];
 }
